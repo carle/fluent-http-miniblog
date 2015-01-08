e02d0771f28c6694edb2dc158fa704968eb4d4b9
Thomas Clavier <tom@tcweb.org>
2015-01-08 16:57:02 +0100

    refacto tests

--- src/test/java/fr/iutinfo/AuthorTest.java
+++ src/test/java/fr/iutinfo/AuthorTest.java
@@ -5,13 +5,18 @@ import org.junit.Test;
 public class AuthorTest extends AbstractTest {
 
 	@Test
-	public void post_form() {
-		/*configure(routes -> routes.post("/create/author", context -> new Payload(
-				"text/plain", context.get("first") + " " + context.get("last"),
-				201)));*/
+	public void postFormToAuthorMustRespond201() {
+		post("/author", "firstname", "yann", "lastname", "secq").should().respond(201);
+	}
 
-		post("/create/author", "firstname", "yann", "lastname", "secq").should().respond(201);
+	@Test
+	public void getAuthor0MustReturnAlan() {
 		get("/author/0").should().contain("alan");
-		get("/author/1").should().contain("yann");
+	}
+	
+	@Test 
+	public void getAuthorAfterInsertMustReturnGoodAuthor() {
+		//Author yann = post("/author", new Author("yann", "secq")).get(Author.class)
+		//get("/author/"+yann.getId()).should().contain("yann");
 	}
 }
